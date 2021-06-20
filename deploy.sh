cb98e108595c6b7c27a2096e640c1ec8fbf1a83b
Emil Vikström <emil.vikstrom@kvdbil.se>
2021-06-20 23:26:49 +0200

    Deploy weathericons

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-rsync -avz --filter=':- .gitignore' . pi@192.168.1.57:magic-carpet/
+rsync -avz --include="weathericons/" --filter=':- .gitignore' . pi@192.168.1.57:magic-carpet/
 ssh -t pi@192.168.1.57 "cd magic-carpet && pip3 install -r requirements.txt"
